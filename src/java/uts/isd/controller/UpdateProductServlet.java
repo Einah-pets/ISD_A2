a05e1e4969696d2be5b77658c67d358086b5c349
LaraStamboulian <79739278+LaraStamboulian@users.noreply.github.com>
2022-05-15 23:25:17 +1000

    Update UpdateProductServlet.java

--- src/java/uts/isd/controller/UpdateProductServlet.java
+++ src/java/uts/isd/controller/UpdateProductServlet.java
@@ -52,11 +52,11 @@ public class UpdateProductServlet extends HttpServlet {
             session.setAttribute("nameErr","Incorrect name format");
             request.getRequestDispatcher("AddProduct.jsp").include(request, response);
         }
-        else if(!validator.validateName(productPrice)){
+        else if(!validator.validatePrice(productPrice)){
             session.setAttribute("nameErr","Incorrect name format");
             request.getRequestDispatcher("AddProduct.jsp").include(request, response);
         }
-        else if(!validator.validateName(productQuantity)){
+        else if(!validator.validateNumber(productQuantity)){
             session.setAttribute("nameErr","Incorrect name format");
             request.getRequestDispatcher("AddProduct.jsp").include(request, response);
 }
