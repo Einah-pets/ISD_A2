13239b84dbbb7c5b43f2da24f1df6ac2d0a7785b
alinestamb <80301435+alinestamb@users.noreply.github.com>
2022-05-15 12:44:51 +1000

    Add files via upload

--- src/java/uts/isd/controller/CreditCardController.java
+++ src/java/uts/isd/controller/CreditCardController.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 import java.sql.Connection;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
 
 import java.util.logging.Level;
 
@@ -25,8 +24,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import javax.servlet.http.HttpSession;
-import uts.isd.model.Order;
-import uts.isd.model.User;
 
 import uts.isd.model.dao.*;
 
@@ -37,32 +34,23 @@ import uts.isd.model.dao.DBManager;
  * @author Wisam
  */
 public class CreditCardController extends HttpServlet {
-
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
         DBManager manager = (DBManager) session.getAttribute("manager");
-
+        
         String nameOnCard = request.getParameter("nameOnCard");
         long creditCardNo = Long.parseLong(request.getParameter("creditCardNo"));
         String expirationDate = request.getParameter("expirationDate");
         int cvv = Integer.parseInt(request.getParameter("cvv"));
-
-        User currentUser = (User) session.getAttribute("user");
-        ArrayList<Order> orders;
-
+        
         try {
             manager.addCreditCard(nameOnCard, creditCardNo, expirationDate, cvv);
-            //reset cart
-            session.setAttribute("cart", null);
-//            //update order history
-//            orders = manager.getAllOrders(currentUser.getUserID());
-//            session.setAttribute("previousOrders", orders);
-            
             request.getRequestDispatcher("paycomplete.jsp").include(request, response);
         } catch (SQLException ex) {
             Logger.getLogger(CreditCardController.class.getName()).log(Level.SEVERE, null, ex);
         }
-
+        
+        
     }
 }
